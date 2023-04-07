cf54a55a2e4f014c3737f4dc98f53fac947e09e8
korg <omwitsa-etole@gmail.com>
2023-04-07 21:54:11 +0300

    1.0

---
+++ main.py
@@ -0,0 +1,30 @@
+"""
+A simple flask server for your app
+"""
+
+from flask import Flask, render_template, request, redirect, url_for, session, abort, flash,Response
+
+app = Flask(__name__)
+
+#all templates are stored in the templates directory relative to main.py file location
+#static files are store in the static directory relative to main.py
+
+"""
+Python is strict on spacing i.e has no curly brackets for function definitions, you have to write clean code to avoid errors
+"""
+
+@app.route("/")
+def home():
+	#add your code or render a template using return render_template(template_name,**locals())
+	pass
+
+@app.route("/booking",methods=["GET","POST"])
+def booking():
+	if request.method == "POST":
+		#if a post request is made to server
+		#have your code here
+		pass
+	pass
+
+if __name__ == "__main__":
+	app.run("0.0.0.0")#run server on all addresses
